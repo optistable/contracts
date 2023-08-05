8c75ee7d77bb94904844611a758d876c5f0392c7
Philipp Braun <3589810@gmail.com>
2023-08-05 14:50:55 +0200

    feat: Update policy manager

--- src/InsuranceManager.sol
+++ src/InsuranceManager.sol
@@ -8,25 +8,46 @@ import {Insurance} from "./Insurance.sol";
 
 contract InsuranceManager is IInsuranceManager {
 
+    address public owner;
+
     struct InsurancePolicy {
         address collateralToken;
         address insuredToken;
         uint256 threshold;
-        Insurance insuranceContract; // The actual contract instance where the policy lives
+        Insurance insuranceContract;
     }
 
     mapping(bytes32 => InsurancePolicy) public policies;
 
-    // Event to notify the creation of a policy
     event PolicyCreated(bytes32 indexed policyId, address collateralToken, address insuredToken, uint256 threshold);
+    
+    constructor() {
+        owner = msg.sender;
+    }
 
-    // Creates a new policy
+    /**
+     * @dev Creates a new insurance policy.
+     * 
+     * @param _collateralToken Address of the collateral token for the new policy
+     * @param _insuredToken Address of the insured token for the new policy
+     * @param _threshold Threshold value for the new policy
+     * @param _depegTermInSeconds Depeg term duration for the insurance
+     * @param _depegThreshold Depeg threshold for the insurance
+     * @param _premiumRate Premium rate for the insurance policy
+     * @param _dataProvider Address of the data provider contract
+     * 
+     * @return policyId Unique identifier for the created policy
+     */
     function createPolicy(
         address _collateralToken, 
         address _insuredToken, 
-        uint256 _threshold
-    ) external override returns (bytes32) {
-        // create unique policy id based on the collateral and insured token and threshold
+        uint256 _threshold,
+        uint256 _depegTermInSeconds,
+        uint256 _depegThreshold,
+        uint256 _premiumRate,
+        address _dataProvider
+    ) external returns (bytes32) {
+        // Create unique policy id based on the collateral and insured token and threshold
         bytes32 policyId = keccak256(abi.encodePacked(_collateralToken, _insuredToken, _threshold));
         
         require(
@@ -36,7 +57,16 @@ contract InsuranceManager is IInsuranceManager {
             "Policy already exists"
         );
         
-        Insurance newInsuranceContract = new Insurance(); // create a new Insurance contract instance (assuming its constructor doesn't require parameters)
+        // Create a new Insurance contract instance
+        Insurance newInsuranceContract = new Insurance(
+            _insuredToken,
+            _collateralToken,
+            _depegTermInSeconds,
+            _depegThreshold,
+            _premiumRate,
+            _dataProvider,
+            address(this) // Management contract is this InsuranceManager contract
+        );
 
         policies[policyId] = InsurancePolicy({
             collateralToken: _collateralToken,
@@ -50,12 +80,22 @@ contract InsuranceManager is IInsuranceManager {
         return policyId;
     }
 
+    /**
+     * @dev Fetches details of an existing insurance policy.
+     * 
+     * @param _policyId Unique identifier of the policy to fetch
+     * 
+     * @return collateralToken Address of the collateral token for the policy
+     * @return insuredToken Address of the insured token for the policy
+     * @return threshold Threshold value of the policy
+     * @return insuranceContract Address of the deployed Insurance contract for this policy
+     */
     function getPolicy(
         bytes32 _policyId
-    ) external view override returns (address, address, uint256, address) {
+    ) external view override returns (address collateralToken, address insuredToken, uint256 threshold, address insuranceContract) {
         require(policies[_policyId].collateralToken != address(0), "Policy does not exist");
 
-        InsurancePolicy memory policy = policies[_policyId];
+        InsurancePolicy storage policy = policies[_policyId];
         return (
             policy.collateralToken,
             policy.insuredToken,
--- src/interfaces/IInsurance.sol
+++ src/interfaces/IInsurance.sol
@@ -1,8 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
+
 pragma solidity ^0.8.13;
 
+/**
+ * @title IInsurance Interface
+ * @dev This interface represents the primary interactions with the Insurance contract.
+ * It includes the structure of Insured and Insurer participants and the main functions
+ * they can execute.
+ */
 interface IInsurance {
-    function isDepegged() external view returns (bool);
-    function recordDepeg() external;
-    function cancelPolicy() external;
-} 
+    function getPossibleInsurerMatches(address insuredAddress) external view returns(address[] memory);
+    function addInsured(uint256 _amount, uint256 _insuranceTimeInSeconds, address insurerAddress) external;
+    function addInsurer(uint256 _amount, uint256 _insuranceTimeInSeconds) external;
+    function claimInsurance() external;
+    function claimPremium() external;
+}

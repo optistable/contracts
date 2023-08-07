7b8527f3172c04f848c60303dec969c8a6af39ef
ferrodri <f.rodriguez.hervias@gmail.com>
2023-08-07 18:28:07 +0200

    add events

--- src/Policy.sol
+++ src/Policy.sol
@@ -7,23 +7,35 @@ import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
 contract Policy is Ownable {
     uint256 public policyCounter = 0;
 
-    // blocknumber => currency => policyId
-    mapping(uint256 => mapping(address => uint256)) public policyId;
+    // blocknumber => currencyInsured => currencyInsurer => policyId
+    mapping(uint256 => mapping(address => mapping(address => uint256))) public policyId;
     // policyId => subscriber => amountAsInsured
     mapping(uint256 => mapping(address => uint256)) public amountAsInsured;
     // policyId => fifoInsured
-    mapping(uint256 =>  address[]) public fifoInsured;
+    mapping(uint256 => address[]) public fifoInsured;
     // policyId => subscriber => amountAsInsurer
     mapping(uint256 => mapping(address => uint256)) public amountAsInsurer;
     // policyId => fifoInsurer
-    mapping(uint256 =>  address[]) public fifoInsurer;
+    mapping(uint256 => address[]) public fifoInsurer;
 
-    function createPolicy(uint256 blockNumber, address currency) public onlyOwner {
-        policyId[blockNumber][currency] = policyCounter;
+    event PolicyCreated(
+        uint256 indexed policyId, uint256 indexed blockNumber, address indexed currencyInsured, address currencyInsurer
+    );
+
+    modifier onlySystemAddress() {
+        require(msg.sender == 0xDeaDDEaDDeAdDeAdDEAdDEaddeAddEAdDEAd0001, "Only system address");
+        _;
+    }
+
+    function createPolicy(uint256 blockNumber, address currencyInsured, address currencyInsurer) public onlyOwner {
+        policyId[blockNumber][currencyInsured][currencyInsurer] = policyCounter;
         policyCounter++;
+
+        emit PolicyCreated(policyCounter - 1, blockNumber, currencyInsured, currencyInsurer);
     }
 
     // Subcribes to an upcoming policy as insured
+    // TODO: frh -> add events
     function subscribeAsInsured(uint256 _policyId, uint256 amount) public {
         amountAsInsured[_policyId][msg.sender] = amount;
         uint256 _fifoLength = fifoInsured[_policyId].length;
@@ -40,5 +52,8 @@ contract Policy is Ownable {
         fifo[_fifoLength] = msg.sender;
         fifoInsurer[_policyId] = fifo;
     }
-}
 
+    function activatePolicy(uint256 _policyId) public onlySystemAddress {}
+
+    function endPolicy(uint256 _policyId) public onlySystemAddress {}
+}
--- test/Policy.t.sol
+++ test/Policy.t.sol
@@ -7,6 +7,11 @@ import "src/Policy.sol";
 contract PolicyTest is Test {
     address public owner = address(50);
     Policy public policy;
+    address public systemAddress = address(0xDeaDDEaDDeAdDeAdDEAdDEaddeAddEAdDEAd0001);
+
+    event PolicyCreated(
+        uint256 indexed policyId, uint256 indexed blockNumber, address indexed currencyInsured, address currencyInsurer
+    );
 
     function setUp() public {
         vm.prank(owner);
@@ -16,14 +21,16 @@ contract PolicyTest is Test {
     function test_CreatePolicy() public {
         assertEq(policy.policyCounter(), 0);
         vm.prank(owner);
-        policy.createPolicy(block.number, address(1));
+        vm.expectEmit(true, true, true, true);
+        emit PolicyCreated(0, block.number, address(1), address(2));
+        policy.createPolicy(block.number, address(1), address(2));
         assertEq(policy.policyCounter(), 1);
-        assertEq(policy.policyId(block.number, address(1)), 0);
+        assertEq(policy.policyId(block.number, address(1), address(2)), 0);
     }
 
     function test_SubscribeAsInsured() public {
         vm.prank(owner);
-        policy.createPolicy(block.number, address(1));
+        policy.createPolicy(block.number, address(1), address(2));
         vm.prank(address(2));
         policy.subscribeAsInsured(0, 100);
         assertEq(policy.amountAsInsured(0, address(2)), 100);
@@ -32,7 +39,7 @@ contract PolicyTest is Test {
 
     function test_SubscribeAsInsurer() public {
         vm.prank(owner);
-        policy.createPolicy(block.number, address(1));
+        policy.createPolicy(block.number, address(1), address(2));
         vm.prank(address(2));
         policy.subscribeAsInsurer(0, 100);
         assertEq(policy.amountAsInsurer(0, address(2)), 100);
@@ -41,6 +48,11 @@ contract PolicyTest is Test {
 
     function test_RevertWhen_CallerIsNotOwner() public {
         vm.expectRevert("Ownable: caller is not the owner");
-        policy.createPolicy(block.number, address(1));
+        policy.createPolicy(block.number, address(1), address(2));
+    }
+
+    function test_RevertWhen_CallerIsNotSystemAddress() public {
+        vm.expectRevert("Only system address");
+        policy.activatePolicy(0);
     }
 }
